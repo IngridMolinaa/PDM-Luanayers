e896055463b4f99fe00e8da05a0fe91f72ed299f
IngridMolinaa <126828042+IngridMolinaa@users.noreply.github.com>
2023-06-05 16:51:38 -0400

    Add files via upload

---
+++ lib/configuracoes.dart
@@ -0,0 +1,141 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'package:flutter/material.dart';
+
+class Configuracoes extends StatelessWidget {
+  const Configuracoes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0.5,
+        
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        title: Row(children: [Text("Ingrid_Luana"), 
+        ],
+        ),
+        actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.video_call_outlined)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.mode_edit_outline_outlined)),
+          ],
+        ),
+
+      
+      body: ListView(children: [
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+        ListTile(
+          title: Text("Pedro"),
+          subtitle: Text("Visto"),
+          leading: CircleAvatar( radius: 26,
+            backgroundImage: NetworkImage(
+                'https://images.unsplash.com/photo-1685725083464-26cab8f2da1e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'),
+          ),
+          trailing: Icon(Icons.camera_alt_outlined, size: 24, color: Colors.grey,),
+          
+        ),
+ 
+        ],
+      ),
+    );
+  }
+}
